de300ac3de8b054caa8f811b3a04177fb7430381
viponedream <viponedream@gmail.com>
2025-04-20 00:41:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'AETSwap – Swap TEA ↔ AET',
-  description: 'Wrap & unwrap TEA to AET on Tea Sepolia testnet',
+  title: 'BBSwap – Swap TEA ↔ BB',
+  description: 'Wrap & unwrap TEA to BB on Tea Sepolia testnet',
   icons: {
     icon: 'images/aet.png',
   },
